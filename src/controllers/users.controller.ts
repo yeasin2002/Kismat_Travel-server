dc1894f7f430a01936d701b1d2c6d453e7e196ef
nahidhasan141400 <nahidhasan141400@gmail.com>
2023-11-13 19:58:23 +0600

    🩹add admin auth mw in get all user fn in user controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -10,6 +10,7 @@ import { join } from "path";
 import { Authorized, Body, Controller, CurrentUser, Delete, Get, Param, Patch, Post, Put, Res, UploadedFile, UseBefore } from "routing-controllers";
 import { OpenAPI } from "routing-controllers-openapi";
 import { Service } from "typedi";
+import { isAdmin } from "@middlewares/isAdmin.middleware";
 
 const avatarFolder = join(__dirname, "../__images/user/avatar");
 
@@ -18,8 +19,8 @@ const avatarFolder = join(__dirname, "../__images/user/avatar");
 export class UserController {
   constructor(public userService: UserService) {}
 
-  @Authorized()
-  @Get()
+  @Post()
+  @UseBefore(isAdmin)
   @OpenAPI({ summary: "Return a list of users" })
   async getUsers() {
     return await this.userService.findAllUser();
