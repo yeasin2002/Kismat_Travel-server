defb6eddb4cfb9eebdbef38b229022d3bc29df88
al-imam <alimam01828@gmail.com>
2023-11-15 18:58:54 +0600

    create method for search statics

---
+++ src/services/statics.service.ts
@@ -0,0 +1,45 @@
+import { db } from "@db";
+import { Op } from "sequelize";
+import { Service } from "typedi";
+
+@Service()
+export class StaticsService {
+  public async createSearch(search: Record<string, any>) {
+    return await db.Searches.create({ search: JSON.stringify(search) });
+  }
+
+  public async getSearches() {
+    return await db.Searches.findAll({ order: [["createdAt", "DESC"]] });
+  }
+
+  public async getSearchesCount() {
+    return await db.Searches.count();
+  }
+
+  public async getTodaysSearches() {
+    const date = new Date();
+    date.setHours(0, 0, 0, 0);
+
+    return await db.Searches.findAll({
+      where: {
+        createdAt: {
+          [Op.gte]: date,
+        },
+      },
+      order: [["createdAt", "DESC"]],
+    });
+  }
+
+  public async getTodaysSearchesCount() {
+    const date = new Date();
+    date.setHours(0, 0, 0, 0);
+
+    return await db.Searches.count({
+      where: {
+        createdAt: {
+          [Op.gte]: date,
+        },
+      },
+    });
+  }
+}
