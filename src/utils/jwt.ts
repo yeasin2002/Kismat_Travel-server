a554376d066855a71ca35a6c9e78d036b98a4504
al-imam <alimam01828@gmail.com>
2023-11-18 18:50:13 +0600

    extends expire days

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,7 +2,7 @@ import { ENV } from "@config";
 import jwt from "jsonwebtoken";
 
 export function sign(payload: Record<string, any>, options?: jwt.SignOptions) {
-  return jwt.sign(payload, ENV.SECRET_KEY, options);
+  return jwt.sign(payload, ENV.SECRET_KEY, Object.assign({ expiresIn: "30d" }, options));
 }
 
 export function verify<T extends Record<string, any> = {}>(token: string, options?: jwt.VerifyOptions) {
